26b79f0fef681bbd2f049235da94aa6de581aa66
Emiliano Lopez <emiliano.lopez2404@gmail.com>
2024-10-01 19:46:41 -0400

    text div setup

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,32 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">Hello World</div>
+    <div className="App">
+      <div className="big-frame">
+        <div className="text">
+          <div className="title">
+            <p id="mediazine">Mediazine</p>
+            <p id="author">By <a target="_blank" href="https://e-Lopz.github.io" rel="noreferrer">Emiliano</a></p>
+          </div>
+          <div className="katatext">
+            <p className="katazine">メディアズィネ</p>
+            <p className="katazine" id="katauthor">エミリアノ</p>
+          </div>
+          <div className="info">
+            <span id="about">about</span>
+            <span id="share">share</span>
+          </div>
+        </div>
+        <div className="poster">
+        </div>
+        <div className="options">
+
+        </div>
+
+      </div>
+
+
+    </div>
   );
 }
 
